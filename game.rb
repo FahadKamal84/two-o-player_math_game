96627800dd8ef0a6baae76313c193397067363e2
Fahad Kamal <fahadkamal@hotmail.com>
2023-12-12 11:09:47 -0500

    implement @players array and use rotate method in order to switch back and forth between players

--- game.rb
+++ game.rb
@@ -3,24 +3,27 @@ class Game
   def initialize
     @player1 = {name: "Picard", lives: 3}
     @player2 = {name: "Kirk", lives: 3 }
+    @players = [@player1, @player2]
     @turn = 1 
 
   end
 
   def play
-    while @player1[:lives] > 0
+    while @player1[:lives] > 0 && @player2[:lives] > 0
+      answering_player = @players.first
       number1 = rand(1..20)
       number2 = rand(1..20)
-      puts "What's is #{number1} plus #{number2} equal?"
+      puts "Hey #{answering_player[:name]}, What's is #{number1} plus #{number2} equal?"
       answer = gets.chomp
       sum = number1 + number2
       if  answer == sum.to_s
         puts "Correct! The answer is #{answer}!"
       else
         puts "I'm sorry. That's wrong!"
-        @player1[:lives] -= 1
-        puts "Lives #{@player1[:lives]}/3 remaining!"
+        answering_player[:lives] -= 1
+        puts "Lives #{answering_player[:lives]}/3 remaining!"
       end
+      @players.rotate!
     end
   end
 
