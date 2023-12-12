f89482db20e5b68aa4acf2ab3136c10f642ea4a2
Fahad Kamal <fahadkamal@hotmail.com>
2023-12-12 10:35:39 -0500

    implement very basic user input logic in play method

--- game.rb
+++ game.rb
@@ -1,14 +1,27 @@
-class game
+class Game
 
   def initialize
-    @player1
-    @player2
+    @player1 = "Picard"
+    @player2 = "Kirk"
     @turn = 1 
 
   end
 
   def play
+    number1 = rand(1..20)
+    number2 = rand(1..20)
+    puts "What's is #{number1} plus #{number2} equal?"
+    answer = gets.chomp
+    sum = number1 + number2
+    if  answer == sum.to_s
+      puts "Correct! The answer is #{answer}!"
+    else
+      puts "I'm sorry. That's wrong!"
+    end
   end
 
 
 end
+
+game_test = Game.new()
+game_test.play
