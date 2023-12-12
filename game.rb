3255e39a239e37fe53ce18eb283d702f91235b1e
Fahad Kamal <fahadkamal@hotmail.com>
2023-12-12 10:42:18 -0500

    implement lives variable to test decrementing livess for each wrong answer in play method

--- game.rb
+++ game.rb
@@ -8,15 +8,20 @@ class Game
   end
 
   def play
-    number1 = rand(1..20)
-    number2 = rand(1..20)
-    puts "What's is #{number1} plus #{number2} equal?"
-    answer = gets.chomp
-    sum = number1 + number2
-    if  answer == sum.to_s
-      puts "Correct! The answer is #{answer}!"
-    else
-      puts "I'm sorry. That's wrong!"
+    lives = 3
+    while lives > 0
+      number1 = rand(1..20)
+      number2 = rand(1..20)
+      puts "What's is #{number1} plus #{number2} equal?"
+      answer = gets.chomp
+      sum = number1 + number2
+      if  answer == sum.to_s
+        puts "Correct! The answer is #{answer}!"
+      else
+        puts "I'm sorry. That's wrong!"
+        lives = lives - 1
+        puts "Lives #{lives}/3 remaining!"
+      end
     end
   end
 
